9a4af8d9af1d3bec2e695fb0c00f0f1583b48e93
Haps <thiamhapstou@gmail.com>
2024-10-01 13:14:05 +0000

    structure de la matrice

---
+++ src/components/matrice.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="tableau-horaires ">
+      <table>
+        <thead>
+          <tr>
+            <th>Horaires</th>
+            <th>Lundi</th>
+            <th>Mardi</th>
+            <th>Mercredi</th>
+            <th>Jeudi</th>
+            <th>Vendredi</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>08:00 - 10:00</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>10:15 - 12:00</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>12:15 - 14:00</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>14:15 -16:00</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>16:15 - 18:00</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  <style scoped>
+  .tableau-horaires {
+    width: 100%;
+   
+  }
+ 
+  .tableau-horaires table {
+    width: 78%;
+    border-collapse: collapse;
+    margin-left: 300px;
+    margin-right: 50px;
+    background-color: white;
+    
+  }
+  
+  .tableau-horaires th,
+  .tableau-horaires td {
+    border: 1px solid #ccc;
+    padding: 10px;
+    text-align: center;
+  }
+  
+  .tableau-horaires th {
+    background-color: #f4f4f4;
+  }
+  </style>
+  
---
+++ src/views/Professeurs/emploisTempsProf.vue
@@ -0,0 +1,23 @@
+<template>
+    <sidebarProf />
+    <topBarProf />
+    <div class="main-content">
+      <matrice />
+    </div>
+  </template>
+  
+  <script setup>
+  import sidebarProf from '@/components/sidebarProf.vue';
+  import topBarProf from '@/components/topBarProf.vue';
+  import matrice from '@/components/matrice.vue';
+
+  </script>
+  
+  
+  <style scoped>
+  .main-content {
+    margin-top: 120px;
+  }
+  
+  
+  </style>

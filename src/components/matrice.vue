88e604a11e36d9eeb4d4dc94045e5b122f425110
Haps <thiamhapstou@gmail.com>
2024-10-01 14:01:34 +0000

    stylisation de la matrice avec entré des données

--- src/components/matrice.vue
+++ src/components/matrice.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="tableau-horaires ">
+    <div class="tableau-horaires">
       <table>
         <thead>
           <tr>
@@ -12,63 +12,55 @@
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>08:00 - 10:00</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>10:15 - 12:00</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>12:15 - 14:00</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>14:15 -16:00</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td>16:15 - 18:00</td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+          <tr v-for="(horaire, index) in horaires" :key="index">
+            <td>{{ horaire.temps }}</td>
+            <td v-for="(jour, dayIndex) in jours" :key="dayIndex">
+              <div v-if="data[dayIndex] && data[dayIndex][index]?.length > 0" class="carte">
+                <div v-for="item in data[dayIndex][index]" :key="item.id">
+                  <h4>{{ item.Matiere }}</h4>
+                  <p>{{ item.classe }}</p>
+                </div>
+              </div>
+              <div v-else>
+                -------
+              </div>
+            </td>
           </tr>
         </tbody>
       </table>
     </div>
   </template>
   
+  <script>
+  export default {
+    props: {
+      horaires: {
+        type: Array,
+        required: true,
+      },
+      data: {
+        type: Array,
+        required: true,
+      },
+      jours: {
+        type: Array,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
   <style scoped>
   .tableau-horaires {
     width: 100%;
-   
   }
- 
+  
   .tableau-horaires table {
     width: 78%;
     border-collapse: collapse;
     margin-left: 300px;
     margin-right: 50px;
     background-color: white;
-    
   }
   
   .tableau-horaires th,
@@ -79,15 +71,37 @@
   }
   
   .tableau-horaires th {
-   font-size: 24px;
-   color: black;
-   font-family: "Poppins", sans-serif; /* Police utilisée */
-    font-weight: 500; 
+    font-size: 24px;
+    color: black;
+    font-family: "Poppins", sans-serif;
+    font-weight: 500;
   }
-  .tableau-horaires td{
+  
+  .tableau-horaires td {
     font-size: 18px;
-    font-family: "Poppins", sans-serif; /* Police utilisée */
-    font-weight: 300; 
+    font-family: "Poppins", sans-serif;
+    font-weight: 300;
+  }
+  
+  .carte {
+    border: 1px solid #ddd;
+    padding: 10px;
+    border-top-left-radius: 20px;
+    border-bottom-right-radius: 20px;
+    background-color: black;
+  }
+  .carte h4{
+    color: white;
+    font-size: 24px;
+    font-family: "Poppins", sans-serif;
+    
+  }
+  .carte p {
+    color: white;
+    font-size: 16px;
+    font-family: "Poppins", sans-serif;
+    margin-top: 5px;
+    text-align: end;
   }
   </style>
   
--- src/views/Professeurs/emploisTempsProf.vue
+++ src/views/Professeurs/emploisTempsProf.vue
@@ -2,15 +2,38 @@
     <sidebarProf />
     <topBarProf />
     <div class="main-content">
-      <matrice />
+      <matrice  :horaires="horaires" :data="donnees" :jours="joursDeLaSemaine" />
+           
     </div>
   </template>
   
   <script setup>
+  import { ref } from 'vue';
   import sidebarProf from '@/components/sidebarProf.vue';
   import topBarProf from '@/components/topBarProf.vue';
   import matrice from '@/components/matrice.vue';
 
+
+
+const horaires = ref([
+  { temps: '08:00 - 10:00' },
+  { temps: '10:15 - 12:00' },
+  { temps: '12:15 - 14:00' },
+  { temps: '14:15 - 16:00' },
+  { temps: '16:15 - 18:00' }
+]);
+
+const joursDeLaSemaine = ref(['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi']);
+
+// Remplacez ceci par vos données réelles
+const donnees = ref([
+  // Exemple de structure de données
+  [ [{ id: 1, Matiere: 'Mathématiques', classe: '6ème A' }, /* ... */], [], [], [], [] ],
+  [ [], [{ id: 1, Matiere: 'Mathématiques', classe: '6ème A' }], [], [], [] ],
+  [ [], [], [{ id: 1, Matiere: 'Mathématiques', classe: '6ème A' }], [], [] ],
+  [ [], [], [], [{ id: 1, Matiere: 'Mathématiques', classe: '6ème A' }], [] ],
+  [ [], [], [], [], [{ id: 1, Matiere: 'Mathématiques', classe: '6ème A' }] ]
+]);
   </script>
   
   
